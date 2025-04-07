f03a82a3ac1baef65c0be5eeeaa8111273982507
Ajaykumar Vijayakumar <ajay.k@ycs.in>
2025-04-08 00:10:54 +0530

    Service file created

---
+++ src/Question.java
@@ -0,0 +1,56 @@
+public class Question {
+    private int id;
+    private String Question;
+    private String Opt1;
+    private String Opt2;
+    private String Opt3;
+    private String Opt4;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return Question;
+    }
+
+    public void setQuestion(String question) {
+        Question = question;
+    }
+
+    public String getOpt1() {
+        return Opt1;
+    }
+
+    public void setOpt1(String opt1) {
+        Opt1 = opt1;
+    }
+
+    public String getOpt2() {
+        return Opt2;
+    }
+
+    public void setOpt2(String opt2) {
+        Opt2 = opt2;
+    }
+
+    public String getOpt3() {
+        return Opt3;
+    }
+
+    public void setOpt3(String opt3) {
+        Opt3 = opt3;
+    }
+
+    public String getOpt4() {
+        return Opt4;
+    }
+
+    public void setOpt4(String opt4) {
+        Opt4 = opt4;
+    }
+}
---
+++ src/QuestionService.java
@@ -0,0 +1,3 @@
+public class QuestionService {
+    Question[] ques = new Question[5];
+}
